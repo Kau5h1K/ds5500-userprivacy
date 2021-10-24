a38145527c270bdd99bd36394657ef6de167100b
Kaushik Nishtala <kaushik@astartemedical.com>
2021-10-23 23:03:15 -0400

    Add relation data creation
    - 7 relation data CSV files
    - ipynb

--- src/starter/main.py
+++ src/starter/main.py
@@ -13,6 +13,7 @@ from src.utils import preprocess
 def main():
      dataset = PrivacyPolicyDataset(cfg)
      X_train, X_dev, X_test, y_train, y_dev, y_test = dataset.splitData(test_size=0.20, dev_size=0.20, has_dev=True, is_majority = False)
+     print("")
 
 if __name__ == '__main__':
     main()
